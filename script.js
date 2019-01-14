186bf819a2f5698e29b06b727f46fcb9492c2d18
Julian Hoyos <jhoyos135@gmail.com>
2019-01-14 14:03:17 -0500

    show images and style them

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ let numberOfGIFs = 10;
 let maxRating = "PG-13";
 let gifContainer = document.querySelector('#gif-container');
 let buttonContainer = document.querySelector('#button-container');
+let title = document.querySelector('.title');
 
 let renderBtns = () => {
+
+
     for(let i in topics) {
         // console.log(topics[i])
         let newButtons = `
             <button class="btn btn-secondary gif-button">
             ${topics[i]}
             </button>
-        `
+        `;
         buttonContainer.innerHTML += newButtons;
 
         //unbind goes here
@@ -22,8 +25,9 @@ let renderBtns = () => {
         document.querySelectorAll('.gif-button').forEach((button) => {
             button.addEventListener('click', (e) => {
 
+                // console.log(e.target.textContent);
+                title.textContent = e.target.textContent;
                 gifContainer.innerHTML = '';
-                // console.log(e.target.textContent)
                 fetch_gif(e.target.textContent);
             });
             button.addEventListener('dblclick', (e) => {
@@ -34,6 +38,11 @@ let renderBtns = () => {
             });
         });
 
+        // let topics_copy = [...topics];
+        // console.log(topics_copy)
+        //TODO: save to local storage
+        // localStorage.setItem('topics_copy', JSON.stringify(topics_copy));
+
     };
 };
 
@@ -43,7 +52,10 @@ let addButton = (show) => {
         buttonContainer.innerHTML = '';
         renderBtns();
     }
+    
     //TODO: save to local storage
+    // localStorage.setItem("topics", JSON.stringify(topics));
+
 };
 
 let fetch_gif = async (show) => {
@@ -52,19 +64,43 @@ let fetch_gif = async (show) => {
     let res = await fetch(url);
     let gifs = await res.json();
     let json = gifs.data;
-
     // console.log(data)
-
     for(let i in json) {
         let data = json[i];
-        console.log(data.images.original.url)
+        // console.log(data)
         //TODO: show gifs here!
+        let img_html = `
+        
+        <div class="each-gif">
+            <img class="gif-image" src="${data.images.fixed_height_still.url}" state="still" still-data=${data.images.fixed_height_still.url} animated-data=${data.images.fixed_height.url}  />
+        </div>
+
+        `;
+
+        gifContainer.innerHTML += img_html;
+
+        document.querySelectorAll('.gif-image').forEach( (gif) => {
+            gif.addEventListener('click', (e) => {
+                // console.log(e.target)
+                let moving = e.target.getAttribute('animated-data');
+                let still = e.target.getAttribute('still-data');
+                // console.log(moving)
+                if(e.target.getAttribute('state') === 'still') {
+                    e.target.setAttribute('state', 'animated');
+                    e.target.setAttribute('src', moving )
+                } else {
+                    e.target.setAttribute('state', 'still');
+                    e.target.setAttribute('src', still)
+                }
+            });
+        });
+
     };
 };
 
 document.addEventListener('DOMContentLoaded', () => {
     renderBtns();
-
+    fetch_gif('hello');
     document.querySelector('#submit').addEventListener('click', (e) => {
         e.preventDefault();
 
@@ -72,9 +108,12 @@ document.addEventListener('DOMContentLoaded', () => {
         // console.log(favorites)
         addButton(favorites.value.trim());
         favorites.value = '';
+        
 
-        //TODO: save to local storage
+        // console.log(saved_topics)
 
+        // saved_topics = JSON.parse(localStorage.getItem("topics"));
+        // console.log(saved_topics)
     });
 
 });
