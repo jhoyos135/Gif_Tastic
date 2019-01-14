77d970740e0dac1da0016882a1348dac4f2b043e
Julian Hoyos <jhoyos135@gmail.com>
2019-01-13 21:28:36 -0500

    added html structure and some js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var url = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+// var topics = ["Rugrats", "Digimon", "Pokemon", "Arthur", "Teen Titans",
+// "Hey Arnold", "Rocket Power", "Spongebob Squarepants", "Recess", "Doug",
+// "Garfield", "Yu Gi Oh", "Ed Ed n Eddy", "The Wild Thornberrys", "Scooby-Doo",
+// "The Simpsons", "DuckTales", "Pinky and the Brain"];
+var numberOfGIFs = 10;
+var cutOffRating = "PG-13";
+
+let show = 'Matrix';
+
+
 
 const fetch_gif = async () => {
 
-    var url = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+    var url = `https://api.giphy.com/v1/gifs/search?q=${show}&api_key=dc6zaTOxFJmzC&rating=${cutOffRating}&limit=${numberOfGIFs}`;
     let res = await fetch(url);
     let gifs = await res.json();
     let json = gifs.data;
@@ -15,4 +24,6 @@ const fetch_gif = async () => {
     }
 }
 
-fetch_gif()
+fetch_gif();
+
+//dc6zaTOxFJmzC
