56e5e8ba55ca13f1385e11cc42444c23ba536280
Julian Hoyos <jhoyos135@gmail.com>
2019-01-13 22:22:17 -0500

    added more scripts to add buttons when added in the input and to change the url when clicked

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-// var topics = ["Rugrats", "Digimon", "Pokemon", "Arthur", "Teen Titans",
-// "Hey Arnold", "Rocket Power", "Spongebob Squarepants", "Recess", "Doug",
-// "Garfield", "Yu Gi Oh", "Ed Ed n Eddy", "The Wild Thornberrys", "Scooby-Doo",
-// "The Simpsons", "DuckTales", "Pinky and the Brain"];
+var topics = ["Sad", "confused", "Love", "Happy", "Chill Out",
+"Cry", "Yes", "No", "Excited", "Sorry",
+"Congratulations", "Sleepy", "Hello", "Ok", "Thank you",
+"Please", "Wink", "Hungry"];
 var numberOfGIFs = 10;
 var cutOffRating = "PG-13";
+let gifContainer = document.querySelector('#gif-container');
+let buttonContainer = document.querySelector('#button-container');
 
-let show = 'Matrix';
+let renderBtns = () => {
+    for(let i in topics) {
+        // console.log(topics[i])
+        let newButtons = `
+            <button class="btn btn-secondary gif-button">
+            ${topics[i]}
+            </button
+        `
+        buttonContainer.innerHTML += newButtons;
 
+        //unbind goes here
 
+        document.querySelectorAll('.gif-button').forEach((button) => {
+            button.addEventListener('click', (e) => {
 
-const fetch_gif = async () => {
+                gifContainer.innerHTML = '';
+                // console.log(e.target.textContent)
+                fetch_gif(e.target.textContent);
+            });
+        });
+
+    };
+};
+
+let addButton = (show) => {
+    if(topics.indexOf(show) === -1) {
+        topics.push(show);
+        buttonContainer.innerHTML = '';
+        renderBtns();
+
+    }
+}
+
+let fetch_gif = async (show) => {
 
     var url = `https://api.giphy.com/v1/gifs/search?q=${show}&api_key=dc6zaTOxFJmzC&rating=${cutOffRating}&limit=${numberOfGIFs}`;
     let res = await fetch(url);
@@ -20,10 +51,19 @@ const fetch_gif = async () => {
 
     for(let i in json) {
         let data = json[i];
-        console.log(data)
+        console.log(data.images.original.url)
     }
 }
 
-fetch_gif();
+document.addEventListener('DOMContentLoaded', () => {
+    renderBtns();
+    document.querySelector('#submit').addEventListener('click', (e) => {
+        e.preventDefault();
 
-//dc6zaTOxFJmzC
+        let favorites = document.querySelector('#favorites').value.trim();
+        // console.log(favorites)
+        addButton(favorites);
+        favorites.value = '';
+
+    });
+});
