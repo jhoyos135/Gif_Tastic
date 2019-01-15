a29b1345b2e33e7961e29128b1dbd0a107615021
Julian Hoyos <jhoyos135@gmail.com>
2019-01-15 13:21:40 -0500

    added pagination

--- script.js
+++ script.js
@@ -3,29 +3,35 @@ let topics = ["Sad", "Confused", "Love", "Happy", "Chill Out",
 "Congratulations", "Sleepy", "Hello", "Ok", "Thank You",
 "Please", "Wink", "Hungry"];
 let numberOfGIFs = 12;
+let offset = 1;
 let maxRating = "pg-13";
 let gifContainer = document.querySelector('#gif-container');
 let buttonContainer = document.querySelector('#button-container');
 let title = document.querySelector('.title');
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev');
 
-// console.log(topics)
+let update_offset = () => {
+    offset = offset + 13;
+}
 
-// localStorage.setItem('topics', JSON.stringify(topics));
+next.addEventListener('click', function() {
+    update_offset();
+    fetch_gif(title.textContent)
+    gifContainer.innerHTML = '';
 
+});
+
+//save to localstorage function
 let SaveDataToLocalStorage = (data) => {
     var a = [];
     a = JSON.parse(localStorage.getItem('topics'));
     a.push(data);
 
     localStorage.setItem('topics', JSON.stringify(a));
-    
 }
 
 let final_topics = JSON.parse(localStorage.getItem('topics'));
-// console.log(final_topics)
-
-
-
 
 let renderBtns = () => {
 
@@ -41,67 +47,56 @@ let renderBtns = () => {
 
         document.querySelectorAll('.gif-button').forEach((button) => {
             button.addEventListener('click', (e) => {
-
+                offset = 1
                 // console.log(e.target.textContent);
                 title.textContent = e.target.textContent;
                 gifContainer.innerHTML = '';
                 fetch_gif(e.target.textContent);
             });
             button.addEventListener('dblclick', (e) => {
-                
-               
 
-                //TODO: save again to local Storage
                 // localStorage.removeItem(e.target.textContent.trim());
                 console.log(e.target.textContent.trim())
-
                 let target_text = e.target.textContent.trim();
-
                 // final_topics.splice( final_topics.indexOf(target_text), 1)
-
                 let target = final_topics.indexOf(target_text)
                 // console.log(target)
-
-                final_topics.splice(target,1)
-    
+                final_topics.splice(target, 1)
                 // console.log(deleteFromArray)
-
                 e.target.remove();
                 console.log(final_topics)
 
                 localStorage.setItem('topics', JSON.stringify(final_topics));
 
-
             });
         });
-
-
     };
-
 };
 
 let addButton = (show) => {
     if(final_topics.indexOf(show) === -1) {
 
+        offset = 1
         final_topics.push(show);
 
         buttonContainer.innerHTML = '';
         console.log(final_topics)
         renderBtns();
 
-        SaveDataToLocalStorage(show)
+        SaveDataToLocalStorage(show);
 
     }
-
 };
 
 let fetch_gif = async (show) => {
-
-    var url = `https://api.giphy.com/v1/gifs/search?q=${show}&api_key=dc6zaTOxFJmzC&rating=${maxRating}&limit=${numberOfGIFs}`;
+    
+    let url = `https://api.giphy.com/v1/gifs/search?q=${show}&api_key=dc6zaTOxFJmzC&rating=${maxRating}&limit=${numberOfGIFs}&offset=${offset}`;
     let res = await fetch(url);
     let gifs = await res.json();
     let json = gifs.data;
-    // console.log(data)
+    
+    // console.log(gifs)
+
     for(let i in json) {
         let data = json[i];
         // console.log(data)
@@ -136,8 +131,7 @@ let fetch_gif = async (show) => {
                 }
             });
         });
-
-    };
+    };  
 };
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -145,26 +139,22 @@ document.addEventListener('DOMContentLoaded', () => {
     renderBtns();
     fetch_gif('hello');
     
+    // when the page loads for the first time load the main topics
     let run = () => {
-        location.reload()
+        location.reload();
         return localStorage.setItem('topics', JSON.stringify(topics));
     }
     if(localStorage.getItem('topics') === null) {
-
         run()
-
     }
-
-
-    // topics.push(final_topics)
 
     document.querySelector('#submit').addEventListener('click', (e) => {
         e.preventDefault();
 
         let favorites = document.querySelector('#favorites');
-        // console.log(favorites)
-
-        // console.log(topics)
+        gifContainer.innerHTML = ''
+        title.textContent = favorites.value;
+        fetch_gif(favorites.value);
         addButton(favorites.value.trim());
         favorites.value = '';
         
